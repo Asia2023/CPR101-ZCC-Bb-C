1e409d4df5d7d105a8181a48664ad71e62bf4fd0
Asia Karki <123264906+Asia2023@users.noreply.github.com>
2023-04-16 18:59:58 -0400

    Add files via upload

---
+++ Version 2/manipulating.h
@@ -0,0 +1,13 @@
+//
+//  manipulating.h
+//  
+//
+//  Created by Asia Karki on 2023-04-16.
+//
+
+#ifndef manipulating_h
+#define manipulating_h
+
+#include <stdio.h>
+
+#endif /* manipulating_h */
